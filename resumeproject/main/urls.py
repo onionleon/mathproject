3b46a79b087550a7cf54ad32b5ebe2e44e472de8
Leon Domagalski <leon.domagalski@me.com>
2022-12-23 19:09:51 +0100

    Added the template for the website, that consists of a nav bar. The template is made using bootstrap. Bootstrap added to the project. Files made for the about us web page, calculus calculator and the algebra calculator.

--- resumeproject/main/urls.py
+++ resumeproject/main/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
 
-    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
+    path("calculus-calculator/", views.calc_calculator, name="calc_calculator"),
+    path("algebra-calculator/", views.alg_calculator, name="alg_calculator"),
+    path("about-us/", views.about_us, name="about_us"),
 
 ]
--- resumeproject/main/views.py
+++ resumeproject/main/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 def home(response):
     return render(response, 'main/base.html', {})
+
+def calc_calculator(response):
+    return render(response, 'main/alg_c.html', {})
+
+def alg_calculator(response):
+    return render(response, 'main/calc_c.html', {})
+
+def about_us(response):
+    return render(response, 'main/about_us.html', {})
